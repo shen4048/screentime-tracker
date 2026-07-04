f151ea0dd7464a0513d24b4daf07415c7e2c5033
shen4048 <shenzhishu12@gmail.com>
2026-07-04 19:02:08 +0800

    Create screentime.js

---
+++ api/screentime.js
@@ -0,0 +1,17 @@
+const data = {};
+
+export default function handler(req, res) {
+  const { app } = req.query;
+  if (!app) return res.status(400).json({ error: 'missing app' });
+
+  const now = Date.now();
+  const cutoff = now - 24 * 60 * 60 * 1000;
+
+  if (!data[app]) data[app] = [];
+  const last = data[app][data[app].length - 1];
+  const action = (!last || last.action === 'close') ? 'open' : 'close';
+  data[app].push({ action, time: now });
+  data[app] = data[app].filter(e => e.time > cutoff);
+
+  res.json({ app, action, time: now, log: data[app] });
+}
